876e348841652b56c75103321f9b77ff37b0d22b
brunoborbadev <brunob_dev@outlook.com>
2019-08-02 16:48:40 -0400

    Add Class ContaBancaria

---
+++ ContaBancaria.cs
@@ -0,0 +1,9 @@
+﻿namespace Exercicio61
+{
+    class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+    }
+}
